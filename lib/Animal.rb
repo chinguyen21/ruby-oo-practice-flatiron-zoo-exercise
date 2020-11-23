c21cdad8fd0c76fdef687a0eb36104a98e60090a
chinguyen21 <nguyen.thimai@northeastern.edu>
2020-11-23 00:32:19 -0500

    commit tree 9eabf27aae0b6062cc9a2e91364eaae92281ae03

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,27 @@
 class Animal
+  attr_accessor :species, :weight, :nickname, :zoo
+
+
+  @@all = []
+  def initialize(species, weight, nickname)
+    @species = species
+    @weight = weight
+    @nickname = nickname 
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def zoo
+    self.zoo
+  end
+
+  def find_by_species(specific_species)
+    Animal.all.map {|animal| animal.species == specific_species}
+  end
+    
+
 
 end
